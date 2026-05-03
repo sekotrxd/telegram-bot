73dc063131f9e334b6710129dec150d38cfd9842
sekotrxd <seko010138@gmail.com>
2026-05-03 08:24:07 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,13 @@
-from telegram.ext import Updater, MessageHandler, Filters
+from telegram import Update
+from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filters
 import os
 
 TOKEN = os.getenv("TOKEN")
 
-def reply(update, context):
-    update.message.reply_text("🔥")
+async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if update.message:
+        await update.message.reply_text("🔥")
 
-updater = Updater(TOKEN, use_context=True)
-
-dp = updater.dispatcher
-dp.add_handler(MessageHandler(Filters.text, reply))
-
-updater.start_polling()
-updater.idle()
+app = ApplicationBuilder().token(TOKEN).build()
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, reply))
+app.run_polling()
