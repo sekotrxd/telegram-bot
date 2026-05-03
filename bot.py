e67ede34aaa92aa05eea2872f4c70939ed1e0899
sekotrxd <seko010138@gmail.com>
2026-05-03 08:19:01 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,8 @@ import os
 TOKEN = os.getenv("TOKEN")
 
 async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("🔥")
+    if update.message:
+        await update.message.reply_text("🔥")
 
 app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(MessageHandler(filters.ALL, reply))
